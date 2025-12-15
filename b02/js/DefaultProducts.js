1ef8d664de59a8a5f9d22d63aeb753f9149b09a5
LEXUANHUNG2002 <le9763263@gmail.com>
2025-12-15 13:46:25 +0900

    Add files via upload

---
+++ b02/js/DefaultProducts.js
@@ -0,0 +1,27 @@
+// Các sản phẩm mặc định (CHỈ dùng để hiển thị)
+const DEFAULT_PRODUCTS = [
+  {
+    code: "A001",
+    name: "和牛ステーキ",
+    price: "2500",
+    stock: "3",
+    img: "../img/beef.png",
+    information: "美味しい"
+  },
+  {
+    code: "A002",
+    name: "唐揚げ弁当",
+    price: "800",
+    stock: "12",
+    img: "../img/karaage.jpg",
+    information: "美味しい"
+  },
+  {
+    code: "A003",
+    name: "サーモン寿司セット",
+    price: "1500",
+    stock: "8",
+    img: "../img/sushi.jpg",
+    information: "美味しい"
+  }
+];
